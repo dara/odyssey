0ab134399ef5477d035de50e9fddb95e1b102e02
matrushka <barisgumustas@gmail.com>
2017-11-29 21:50:57 +0100

    Added deploy script.

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+# Build the app
+npm run build
+# Copy git context to the build directory
+cp -R .git build/.git 
+# Get into the build directory
+pushd build
+# Add and commit changes
+git add . 
+git commit -am \"Publishing...\" 
+# Push the current branch to gh-pages branch.
+# remote can be changed with REMOTE environment var ("origin" by default).
+git push -u ${REMOTE:-origin} master:gh-pages --force
+# Get out of the build directory
+popd
+# Cleanup the build directory
+rm -rf build
+mkdir -p build
